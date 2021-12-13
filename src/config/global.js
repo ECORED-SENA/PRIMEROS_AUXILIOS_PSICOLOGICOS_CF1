fa5189171a7ff7bc415bd1c9164b83ac896ab0ef
Daniel Mutis <64042203+DanielMutis85@users.noreply.github.com>
2021-12-13 17:57:37 -0500

    Ajuste

--- src/config/global.js
+++ src/config/global.js
@@ -147,7 +147,7 @@ export default {
     subMenu: [
       {
         icono: 'fas fa-sitemap',
-        titulo: 'Sintesis',
+        titulo: 'Síntesis',
         nombreRuta: 'sintesis',
       },
       {
